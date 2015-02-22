89e9eb59b25bab8ecd840fadd74d59bf7f7fc0e9
iridiumcao <iridiumcao@gmail.com>
2015-02-22 10:47:42 +0800

    helloworld of hibernate

---
+++ helloworldofhibernate/src/org/iridium/hlearning/HelloWorld.java
@@ -0,0 +1,40 @@
+package org.iridium.hlearning;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.iridium.hlearning.entity.Event;
+
+import java.util.Date;
+import java.util.List;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //////////
+//        Configuration configuration = new Configuration();  
+//        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();  
+//        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);  
+        //////////
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Event("Our very first event! 第一条记录", new Date()));
+        session.save(new Event("A follow up event! 第二条记录", new Date()));
+        session.getTransaction().commit();
+        session.close();
+
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        List result = session.createQuery(" from Event").list();
+        result.forEach((e) -> {
+            Event event = (Event) e;
+            System.out.println("Event (" + event.getDate() + "): " + event.getTitle());
+        });
+        session.getTransaction().commit();
+        session.close();
+
+        sessionFactory.close();
+    }
+
+}
