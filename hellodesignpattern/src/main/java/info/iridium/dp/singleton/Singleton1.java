b0786e2beb286a51fe5d7f16a304ccba48b9818d
Iridium <iridiumcao@gmail.com>
2016-09-05 16:39:25 +0800

    Update Singleton pattern

--- hellodesignpattern/src/main/java/info/iridium/dp/singleton/Singleton1.java
+++ hellodesignpattern/src/main/java/info/iridium/dp/singleton/Singleton1.java
@@ -2,6 +2,10 @@ package info.iridium.dp.singleton;
 
 /**
  * 饿汉单例模式
+ * 
+ * 这种写法的缺陷是，它不是懒加载的（lazy initialization）的，一旦开始加载类，实例就创建了。而且，这种写法可能无法适用某些场景：比如对象要通过
+ * 配置文件创建，而且需要几个参数。
+ * 
  * <p/>
  * Created by caoi on 2015/3/2.
  */
--- hellodesignpattern/src/main/java/info/iridium/dp/singleton/Singleton2.java
+++ hellodesignpattern/src/main/java/info/iridium/dp/singleton/Singleton2.java
@@ -2,6 +2,9 @@ package info.iridium.dp.singleton;
 
 /**
  * 懒汉单例模式
+ * 
+ * 这种写法的缺陷在于可能有线程同步问题。
+ * 
  * Created by caoi on 2015/3/2.
  */
 public class Singleton2 {
